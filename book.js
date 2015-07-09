80dbf523f02d0f5fdd31f6bedf0968fb841a54d1
Sredna Kunowski <srednakun@gmail.com>
2015-07-09 10:09:27 -0700

    Update book.js

--- book.js
+++ book.js
@@ -1,7 +1,5 @@
 //Author: Sredna Kunowski 7/3/2015
 
-//This is the Book class. It returns information about books, like the title and author.
-
 function Book (title, author) 
 {
 	this.bookAuthor = author;
